c98305722e2c4cf22dc873d8839fdc748db66f53
BIT Student <classroom@bgit.rs>
2020-06-08 17:38:02 +0200

    news

---
+++ hacker-news/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/Header.js'
+import News from './components/News/News.js';
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <News />
+    </div>
+  );
+}
+
+export default App;
